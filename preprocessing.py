de9e1659deea823b164c66b5898300ccc1700a40
Lais Isabelle Alves dos Santos <lais.dossantos@telecom-paris.fr>
2023-05-31 11:19:58 +0200

    adjusting functions of pre-processing

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ import os
 import nibabel as nib
 import matplotlib.pyplot as plt
 import re
+import numpy as np
 
 def nii_reader(path):
     """
@@ -10,17 +11,29 @@ def nii_reader(path):
     nii = nib.load(path)
     return nii.get_fdata()
 
-def visualize_image_mask(image, mask):
+def visualize_image_mask(image, mask, depth_size):
     """
     Visualize an image and its mask
     """
-    fig, ax = plt.subplots(1, 2, figsize=(10, 10))
-    ax[0].imshow(image, cmap='gray')
-    ax[1].imshow(mask, cmap='gray')
+
+    # Visualize the slices of the image
+    fig, ax = plt.subplots(1, depth_size, figsize=(25, 8))
+    for j in range(depth_size):
+        ax[j].imshow(image[:,:,j], cmap='gray')  # show one single slice of each image
+        ax[j].axis('off')
+        ax[j].set_title('Slice: {}'.format(j))
+    plt.show()
+
+    # Visualize the slices of the ground truth image
+    fig, ax = plt.subplots(1, depth_size, figsize=(25, 8))
+    for j in range(depth_size):
+        ax[j].imshow(mask[:,:,j], cmap='gray')  # show one single slice of each image
+        ax[j].axis('off')
+        ax[j].set_title('Slice: {}'.format(j))
     plt.show()
 
 
-def preprocess_files_acdc(folder, nb_files):
+def preprocess_files_acdc(folder, nb_files, test=False):
     """
     Load the images and masks from the ACDC dataset
     """
@@ -29,7 +42,12 @@ def preprocess_files_acdc(folder, nb_files):
     masks_ED = []
     masks_ES = []
 
-    for i in range(1, nb_files + 1):
+    if test:
+        start = 101
+    else:
+        start = 1
+
+    for i in range(start, nb_files + start):
         files_folder = []
         frame_number = []
         patient_folder = os.path.join(folder, 'patient' + str(i).zfill(3))
@@ -62,3 +80,43 @@ def preprocess_files_acdc(folder, nb_files):
                         images_ES.append(os.path.join(patient_folder, file))
 
     return images_ED, masks_ED, images_ES, masks_ES
+
+def heart_mask_extraction(masks_patients):
+    """
+    Extract each cavity of the heart and the myocardium.
+    Eliminates other elements that are not the desired ones.
+
+    Parameters:
+    -----------
+    `images_seg`: segmented images
+
+    Returns:
+    --------
+    `masks_rv`: mask for the right ventricle
+    `masks_myo`: mask for the myocardium
+    `masks_lv`: mask for the left ventricle
+    """
+
+    nb_patients = len(masks_patients)
+    masks_rv = []
+    masks_myo = []
+    masks_lv = []
+
+    # Iterate through the patients to get their masks
+    for i in range(nb_patients):
+        masks = nii_reader(masks_patients[i])
+        seg_rv = []
+        seg_myo = []
+        seg_lv = []
+
+        # Take the mask for each slice of the patient
+        for mask in masks:
+            seg_rv.append(np.where(mask == 1, 1, 0))
+            seg_myo.append(np.where(mask == 2, 1, 0))
+            seg_lv.append(np.where(mask == 3, 1, 0))
+
+        masks_rv.append(seg_rv)    
+        masks_myo.append(seg_myo)
+        masks_lv.append(seg_lv)
+
+    return masks_rv, masks_myo, masks_lv
