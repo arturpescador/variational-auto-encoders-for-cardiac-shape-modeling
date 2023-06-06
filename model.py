548ac1b1918ffd87e728f5d72eb5e53f515ff98d
Artur Pescador <artur.pescador@hotmail.com>
2023-06-06 22:13:56 +0200

    vae - model

--- model.py
+++ model.py
@@ -2,27 +2,99 @@ import tensorflow as tf
 import numpy as np
 import torch
 import torch.nn as nn 
+import torch.nn.functional as F
+import torch.optim as optim
 
-def soft_dice_loss(y_true, y_pred, smooth=1e-6):
-    """
-    Calculate the soft Dice loss between the ground truth and predicted masks.
+class VAE(nn.Module):
+    def __init__(self, x_dim, z_dim):
+        super(VAE, self).__init__()
 
-    Parameters
-    ----------
-    `y_true` : tensor, the ground truth mask.
+        self.z_dim = z_dim
+        
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels=x_dim, out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU()
+        )
+
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=32, out_channels=x_dim, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.Sigmoid()
+        )
+
+        # Latent space layers
+        self.fc_mu = nn.Linear(32 * 7 * 7, z_dim)
+        self.fc_log_var = nn.Linear(32 * 7 * 7, z_dim)
+
+    def encode(self, x):
+        h = self.encoder(x)
+        h = h.view(h.size(0), -1)  # Flatten
+        return self.fc_mu(h), self.fc_log_var(h)
+
+    def decode(self, z):
+        h = self.decoder_input(z)
+        h = h.view(h.size(0), 32, 7, 7)  # Reshape
+        return self.decoder(h)
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+    def forward(self, x):
+        """
+        Forward pass through the network.
+        
+        Parameters
+        ----------
+        `x` : tensor, the input image.
+        
+        Returns
+        -------
+        `x_hat` : tensor, the reconstructed image.
+        """
+        mu, log_var = self.encoder(x)
+        z = self.sampling(mu, log_var)
+        return self.decoder(z), mu, log_var
     
-    `y_pred` : tensor, the predicted mask.
+    def soft_dice_loss(self, y_pred, y_true):
+        """
+        Calculate the soft Dice loss between the ground truth and predicted masks.
 
-    `smooth` : float, to avoid division by zero. Default is `1e-6`.
-    """
-    intersection = tf.reduce_sum(y_true * y_pred, axis=[1,2,3])
-    union = tf.reduce_sum(y_true + y_pred, axis=[1,2,3])
+        Parameters
+        ----------
+        `y_true` : tensor, the ground truth mask.        
+        `y_pred` : tensor, the predicted mask.
+        """
+        smooth = 1.0
+        intersection = torch.sum(y_pred * y_true)
+        dice_coeff = (2.0 * intersection + smooth) / (torch.sum(y_pred) + torch.sum(y_true) + smooth)
+        loss = 1.0 - dice_coeff
+        return loss
 
-    dice_coef = (2. * intersection + smooth) / (union + smooth)
-    dice_loss = 1 - tf.reduce_mean(dice_coef)
+    def loss_function(self, y_pred, y_true, mu, log_var):
+        """
+        Calculate the loss function for the VAE.
 
-    return dice_loss
+        Parameters
+        ----------
+        `y_true` : tensor, the ground truth mask.
+        `y_pred` : tensor, the predicted mask.
+        `mu` : tensor, the mean of the latent space.
+        `log_var` : tensor, the log variance of the latent space.
 
+        Returns
+        -------
+        `loss` : tensor, the loss value.
+        """
+        reconstruction_error = self.soft_dice_loss(y_pred, y_true)
+        KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+        return (reconstruction_error + KLD) / y_pred.size(0)
 
 ## Architecture
 # We will use the following convoluional architecture for the classifier:
@@ -35,7 +107,7 @@ def soft_dice_loss(y_true, y_pred, smooth=1e-6):
 # - Flatten
 # - Dense layer
 
-
+"""
 def class_model_vao(n_epochs, batch_size, learning_rate, nb_classes):
 
     # number of convolutional filters to use
@@ -68,4 +140,4 @@ def class_model_vao(n_epochs, batch_size, learning_rate, nb_classes):
                                                     nn.Linear(n_h_2*(imgs_generated.shape[2]-1)*(imgs_generated.shape[3]-1), nb_classes)) # FILL IN CODE HERE
 
     criterion = nn.soft_dice_loss() # FILL IN CODE HERE
-    optimizer = torch.optim.Adam(classification_model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(classification_model.parameters(), lr=learning_rate)"""
